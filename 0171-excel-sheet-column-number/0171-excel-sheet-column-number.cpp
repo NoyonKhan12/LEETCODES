345c9a80c5d02c7e9013c1a9472d758cb722cb31
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-01-24 21:52:33 +0600

    Time: 10 ms (5.67%), Space: 6 MB (70.05%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) 
+    {
+        int res = 0;
+        
+        for(char c:columnTitle)
+        {
+            int x = c - 'A' + 1;
+            res = res * 26 + x;
+        }
+        return res;
+    }
+};
