07e4bff7243c7119ce7b1cf8450170e54afcbeb9
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-01-26 23:19:14 +0600

    Time: 7 ms (86.87%), Space: 18.9 MB (18.68%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root)
+    {
+        if(!root)
+        {
+            return 0;
+        }
+        
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        
+        return (max(maxLeft, maxRight) + 1);
+        
+    }
+};
