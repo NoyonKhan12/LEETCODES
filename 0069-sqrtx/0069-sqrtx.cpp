89e60e7f80cf878b0f0cd27ebaccc8b50ee66a92
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-02-01 17:43:36 +0600

    Time: 0 ms (100.00%), Space: 6 MB (7.53%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x)
+    {
+        int l = 0;
+        long long r = x;
+        
+        while(l < r)
+        {
+            long long mid = l + (r - l + 1) / 2;
+            
+            if(mid * mid <= (long long)x)
+            {
+                l = mid;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+};
