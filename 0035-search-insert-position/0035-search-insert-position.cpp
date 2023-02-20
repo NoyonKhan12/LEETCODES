4cf7355e731ada7a67ec5c042bdd27a28a155c60
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-02-20 12:00:10 +0600

    Time: 0 ms (100.00%), Space: 9.8 MB (31.02%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {     
+        auto t = lower_bound(nums.begin(),nums.end(),target);
+        return (t - nums.begin());        
+    }
+};
