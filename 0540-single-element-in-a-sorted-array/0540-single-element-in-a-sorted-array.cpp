19a222afdd3369b296de2567c00c2c1d0ce1d9cc
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-02-21 11:40:37 +0600

    Time: 23 ms (91.47%), Space: 22.4 MB (62.74%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums)
+    {
+        int x = 0;
+        
+        for(auto i:nums)
+        {
+            x = x ^ i;
+        }
+        return x;
+    }
+};
