f86225094c6dd0122a54cfd366412c0f74701ef0
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-02-21 13:05:17 +0600

    Time: 5 ms (69.91%), Space: 7.9 MB (53.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     vector<int> countBits(int n)
     {
-        int cnt;
-        vector<int> v;
-        char ch = '1';
+        vector<int> result(n+1, 0);
         
-        for(int i=0; i<=n; i++)
-        {            
-            string str = bitset<32>(i).to_string();
-            cnt = count(str.begin(), str.end(), ch);
-            v.push_back(cnt);
+        for(int i=1; i<=n; i++)
+        {
+            result[i] = result[i >> 1] + (i & 1);
         }
-        return v;        
+        return result;       
     }
 };
