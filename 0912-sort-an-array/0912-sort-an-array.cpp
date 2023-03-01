f0d9b1ca4ff28d6094327cd731cb84efeeb504d0
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-03-01 10:22:03 +0600

    Time: 120 ms (95.13%), Space: 65 MB (39.83%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums)
+    {
+        int minVal = *min_element(nums.begin(), nums.end());
+        int maxVal = *max_element(nums.begin(), nums.end());
+        
+        bool isAscending = true;
+        int freqSize = maxVal - minVal + 1 + 1, size = nums.size();
+        vector<int> freq(freqSize, 0), sorted(size, 0);
+        
+        for(int ind=0; ind<size; ind++)
+        {
+            freq[nums[ind] - minVal]++;
+        }
+        
+        if(isAscending)
+        {
+            for(int ind=1; ind<freqSize; ind++)
+            {
+                freq[ind] += freq[ind - 1];
+            }
+        }
+        else
+        {
+            for(int ind=freqSize-2; ind>=0; ind--)
+            {
+                freq[ind] += freq[ind + 1];
+            }
+        }
+       
+        for(int ind=size-1; ind>=0; ind--)
+        {
+            sorted[freq[nums[ind] - minVal]-- - 1] = nums[ind];
+        }
+        nums = sorted;
+        return nums;
+    }
+};
