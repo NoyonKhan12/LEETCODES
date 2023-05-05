c36cbcfc03bdb72a779e6bfb821e5b15b45bd8b7
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-05-05 23:11:27 +0600

    Time: 21 ms (69.85%), Space: 9.9 MB (95.70%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -4,11 +4,12 @@ public:
     {
         int count = 0;
         int ans = 0;
+        int n = s.size();
         
         bool vowels[26] = {false};
         vowels[0] = vowels[4] = vowels[8] = vowels[14] = vowels[20] = true;
 
-        for(int i=0; i < s.length(); i++) 
+        for(int i=0; i<n; i++) 
         {           
             if(i >= k && vowels[s[i-k]-'a']) 
             {
