c23617d447f454054b6453182ba78e2647cfc85e
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-05-12 23:55:32 +0600

    Time: 411 ms (14.83%), Space: 115.3 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -3,20 +3,11 @@ public:
     long long mostPoints(vector<vector<int>>& questions) 
     {
         int n = questions.size();
-        vector<long long> dp(n, 0ll);
-        dp[n-1] = questions[n-1][0];
+        vector<long long> dp(n+1, 0);
         
-        for(int i=n-2; i>=0; i--)
+        for(int i=n-1; i>=0; i--)
         {
-            int points = questions[i][0];
-            int bp = questions[i][1] + 1;
-            long long tot_points = points;
-            
-            if(i+bp < n)
-            {
-                tot_points += dp[i+bp];
-            }
-            dp[i] = max(dp[i+1], tot_points);
+            dp[i] = max(dp[i+1], dp[min(n, i+questions[i][1]+1)] + questions[i][0]);
         }
         return dp[0];
     }
