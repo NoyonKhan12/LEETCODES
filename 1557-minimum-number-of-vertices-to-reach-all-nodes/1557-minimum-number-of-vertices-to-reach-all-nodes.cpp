a30561e6371d8fa75f09794edd95b5aed5e1232f
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-05-19 00:09:54 +0600

    Time: 302 ms (68.58%), Space: 94.3 MB (74.73%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) 
     {
-        set<int> st;
+        vector<int> v;
+        vector<int> visited(n, 0);
+        
         int x = edges.size();
         
         for(int i=0; i<x; i++)
         {
-            st.insert(edges[i][0]);
-            st.insert(edges[i][1]);
+            visited[edges[i][1]] = 1;
         }
         
-        for(auto i:edges)
+        for(int i=0; i<n; i++)
         {
-            if(st.find(i[1]) != st.end())
+            if(visited[i] == 0)
             {
-                st.erase(i[1]);
+                v.push_back(i);
             }
         }
-        vector<int> v(st.begin(), st.end());
         return v;
     }
 };
