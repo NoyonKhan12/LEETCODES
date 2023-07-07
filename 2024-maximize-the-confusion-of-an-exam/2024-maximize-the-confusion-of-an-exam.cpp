a2c18b032f7ab4edf7bb7a4704a9591ad996a1c2
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2023-07-07 22:13:28 +0600

    Time: 27 ms (69.05%), Space: 11.9 MB (20.67%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    int count(string s, int k, char ch)
+    {
+        int n = s.length();
+        
+        int maxlen = 0, bad = 0; 
+        int i = 0, j = 0;
+        
+        while(j < n)
+        {
+            if(s[j] != ch) 
+            {
+                bad++;
+            }
+            
+            while(i <= j && bad > k)
+            {
+                char cur = s[i];
+                if(cur != ch) 
+                {
+                    bad--; 
+                }
+                i++;
+            }
+            
+            maxlen = max(maxlen, j-i+1);
+            j++;
+        }
+        return maxlen;
+    }
+    
+    int maxConsecutiveAnswers(string answerKey, int k) 
+    {
+        return max(count(answerKey, k, 'T'), count(answerKey, k, 'F'));
+    }
+};
