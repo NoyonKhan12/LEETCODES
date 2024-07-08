a9cc0570f165e436c3aab11dd00e39a10b38e175
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2024-07-08 23:56:36 +0600

    Time: 27 ms (15.66%), Space: 25.7 MB (9.19%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) 
+    {
+        queue<int> q;
+        
+        for(int i=1; i<=n; i++)
+        {
+            q.push(i);
+        }
+        
+        while(q.size() != 1)
+        {
+            for(int i=1; i<=k; i++)
+            {
+                if(i != k)
+                {
+                    int x = q.front();
+                    q.pop();
+                    q.push(x);
+                }
+                
+                if(i == k) q.pop();
+            }
+        }
+        return q.front();
+    }
+};
