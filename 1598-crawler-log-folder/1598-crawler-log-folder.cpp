019b786e121d700d1735aa202e1f73f3370c41c8
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2024-07-10 21:34:24 +0600

    Time: 3 ms (86.11%), Space: 13.8 MB (84.88%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -6,13 +6,8 @@ public:
         
         for(auto i:logs)
         {
-            if(i.size() == 3 && i[0] == '.' && i[1] == '.' && i[2] == '/')
-            {
-                x = x > 0 ? x-1 : 0;
-            }
-            
+            if(i.size() == 3 && i[0] == '.' && i[1] == '.' && i[2] == '/') x = x > 0 ? x-1 : 0;
             else if(i.size() == 2 && i[0] == '.' && i[1] == '/') continue;
-            
             else x++;
         }
         return x;
