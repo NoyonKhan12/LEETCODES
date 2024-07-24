f16c68478610ef4e8045906172b90b9a9f04361e
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2024-07-25 00:49:06 +0600

    Time: 657 ms (19.05%), Space: 113.1 MB (29.00%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums)
+    {
+        vector<int> res;
+        multimap<long long, int> mp;
+        
+        for(auto i:nums)
+        {
+            int num = i;
+            string str;
+            
+            if(i == 0)
+            {
+                str = to_string(mapping[0]);
+            }
+            else
+            {
+                 while(num > 0)
+                {
+                    int x = num % 10;
+                    int y = mapping[x];
+                    str = to_string(y) + str;
+                    num = num / 10;
+                }
+            }
+           
+            long long z = stoll(str);
+            mp.insert({z, i});
+        }
+        
+        for(auto i:mp)
+        {
+            res.push_back(i.second);
+        }
+        
+        return res;
+    }
+};
