0993779b20d3a04378f7ec5d9e4ae88b4e2e3a5e
Md. Nuruzzaman Noyon <87333310+NoyonKhan12@users.noreply.github.com>
2024-08-16 20:44:34 +0600

    Time: 231 ms (59.91%), Space: 107.9 MB (29.72%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -5,21 +5,18 @@ public:
     {
         int n = arrays.size();
         int maxDiff = 0, x, y;
-        int a = arrays[0].size() - 1;
         int mini = arrays[0][0];
-        int maxi = arrays[0][a];
+        int maxi = arrays[0].back();
 
         for(int i=1; i<n; i++)
         {
-            a = arrays[i].size() - 1;
-            
-            x = abs(arrays[i][a] - mini);
-            y = abs(arrays[i][0] - maxi);
-            
-            mini = min(mini, arrays[i][0]);
-            maxi = max(maxi, arrays[i][a]);
+            x = abs(arrays[i][0] - maxi);
+            y = abs(arrays[i].back() - mini);
             
             maxDiff = max(maxDiff, max(x, y));
+            
+            mini = min(mini, arrays[i][0]);
+            maxi = max(maxi, arrays[i].back());
         }
         return maxDiff;
     }
